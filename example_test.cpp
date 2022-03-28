d6ab33df0fb0b3a5ac9cce9a736e23f6450249c0
danxxo <90875721+danxxo@users.noreply.github.com>
2022-03-28 12:52:44 +0300

    update quick_test

--- example_test.cpp
+++ example_test.cpp
@@ -2,14 +2,14 @@
 #include <string>
 #include "sorts.cpp"
 
-
-TEST_F(SortTest, swap){
+TEST_F(SortTest, swap) {
     long a = 15;
     long b = 100;
-    swap(a,b);
-    EXPECT_EQ(a,100);
-    EXPECT_EQ(b,15);
+    swap(a, b);
+    EXPECT_EQ(a, 100);
+    EXPECT_EQ(b, 15);
 }
+
 TEST_F(SortTest, insertion_10) {
     arr_10;
     insertion_sort(arr_10.begin(), arr_10.end());
@@ -17,6 +17,7 @@ TEST_F(SortTest, insertion_10) {
         EXPECT_TRUE(arr_10[i - 1] <= arr_10[i]);
     }
 }
+
 TEST_F(SortTest, insertion_10000) {
     arr_10000;
     size_t vec_size = arr_10000.size();
@@ -25,43 +26,73 @@ TEST_F(SortTest, insertion_10000) {
         EXPECT_TRUE(arr_10000[i - 1] <= arr_10000[i]);
     }
 }
-TEST_F(SortTest, bubblesort_10){
+
+
+TEST_F(SortTest, bubblesort_10) {
     arr_10;
     size_t vec_size = arr_10.size();
     puzir_sort(arr_10, vec_size);
-    for(int i = 1; i < vec_size; i++){
-        EXPECT_TRUE(arr_10[i-1] <= arr_10[i]);
-    }
-}
-TEST_F(SortTest, bubblesort_10000){
-    arr_10000;
-    size_t vec_size = arr_10000.size();
-    puzir_sort(arr_10000, vec_size);
-    for(int i = 1; i < vec_size; i++){
-        EXPECT_TRUE(arr_10000[i-1] <= arr_10000[i]);
+    for (int i = 1; i < vec_size; i++) {
+        EXPECT_TRUE(arr_10[i - 1] <= arr_10[i]);
     }
 }
 
-TEST_F(SortTest, quicksort_10){
+TEST_F(SortTest, bubblesort_10000) {
+    arr_10000;
+    size_t vec_size = arr_10000.size();
+    puzir_sort(arr_10000, vec_size);
+    for (int i = 1; i < vec_size; i++) {
+        EXPECT_TRUE(arr_10000[i - 1] <= arr_10000[i]);
+    }
+}
+
+TEST_F(SortTest, quicksort_10_array_size) {
     arr_10;
     size_t vec_size = arr_10.size();
     quicksort(arr_10, 0, arr_10.size() - 1);
-    for(int i = 1; i < vec_size; i++){
-        EXPECT_TRUE(arr_10[i-1] <= arr_10[i]);
+    for (int i = 1; i < vec_size; i++) {
+        EXPECT_TRUE(arr_10[i - 1] <= arr_10[i]);
     }
 }
-TEST_F(SortTest, quicksort_10000){
+
+TEST_F(SortTest, quicksort_10000_array_size) {
     arr_10000;
-    size_t vec_size = arr_10.size();
-    quicksort(arr_10, 0, arr_10.size() - 1);
-    for(int i = 1; i < vec_size; i++){
-        EXPECT_TRUE(arr_10[i-1] <= arr_10[i]);
+    size_t vec_size = arr_10000.size();
+    quicksort(arr_10000, 0, arr_10000.size() - 1);
+    for (int i = 1; i < vec_size; i++) {
+        EXPECT_TRUE(arr_10000[i - 1] <= arr_10000[i]);
     }
 }
-TEST_F(SortTest, merge10){
+
+TEST_F(SortTest, quicksort_10_) {
+    arr_10;
+    quicksort(arr_10.begin(), arr_10.end());
+    for (int i = 1; i < 10; i++) {
+        EXPECT_TRUE(arr_10[i - 1] <= arr_10[i]);
+    }
+}
+
+TEST_F(SortTest, quicksort_10000_) {
+    arr_10000;
+    quicksort(arr_10000.begin(), arr_10000.end());
+    for (int i = 1; i < 10; i++) {
+        EXPECT_TRUE(arr_10000[i - 1] <= arr_10000[i]);
+    }
+}
+
+
+TEST_F(SortTest, merge10) {
     arr_10;
     merge_sort(arr_10.begin(), arr_10.end());
-    for(int i = 1; i < arr_10.size(); i++){
-        EXPECT_TRUE(arr_10[i-1] <= arr_10[i]);
+    for (int i = 1; i < arr_10.size(); i++) {
+        EXPECT_TRUE(arr_10[i - 1] <= arr_10[i]);
+    }
+}
+
+TEST_F(SortTest, merge10000) {
+    arr_10000;
+    merge_sort(arr_10000.begin(), arr_10000.end());
+    for (int i = 1; i < arr_10000.size(); i++) {
+        EXPECT_TRUE(arr_10000[i - 1] <= arr_10000[i]);
     }
 }
