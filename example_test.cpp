b052bf6f46927cf974be18a52f7d41e73c2751dd
danxxo <90875721+danxxo@users.noreply.github.com>
2022-03-28 11:39:29 +0300

    update insertoin_tests

--- example_test.cpp
+++ example_test.cpp
@@ -10,22 +10,19 @@ TEST_F(SortTest, swap){
     EXPECT_EQ(a,100);
     EXPECT_EQ(b,15);
 }
-
-TEST_F(SortTest, insertion_10){
+TEST_F(SortTest, insertion_10) {
     arr_10;
-    size_t vec_size = arr_10.size();
-    insertion_sort(arr_10, vec_size);
-    for(int i = 1; i < vec_size; i++){
-        EXPECT_TRUE(arr_10[i-1] <= arr_10[i]);
+    insertion_sort(arr_10.begin(), arr_10.end());
+    for (int i = 1; i < arr_10.size(); i++) {
+        EXPECT_TRUE(arr_10[i - 1] <= arr_10[i]);
     }
 }
-
-TEST_F(SortTest, insertion_10000){
+TEST_F(SortTest, insertion_10000) {
     arr_10000;
     size_t vec_size = arr_10000.size();
-    insertion_sort(arr_10000, vec_size);
-    for(int i = 1; i < vec_size; i++){
-        EXPECT_TRUE(arr_10000[i-1] <= arr_10000[i]);
+    insertion_sort(arr_10000.begin(), arr_10000.end());
+    for (int i = 1; i < vec_size; i++) {
+        EXPECT_TRUE(arr_10000[i - 1] <= arr_10000[i]);
     }
 }
 TEST_F(SortTest, bubblesort_10){
